3b3278850e6271c5f0b6cbcacdbbcb50a650c5bb
Prelo Tulshi <prelotulshi@gmail.com>
2024-04-26 16:46:35 +0530

    mui color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "@fontsource/roboto"; // Load Roboto font (used by MUI)
+import { Card, CardHeader, Switch, CardContent, Box, Container, Typography, FormGroup, FormControlLabel, CssBaseline, createTheme, ThemeProvider, colors, } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+const lightTheme = createTheme({
+  palette: {
+    primary: {
+      main: colors.grey[500]
+    }
+  }
+})
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: colors.orange[500]
+    }
+  },
+})
+
+const App: React.FC = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const changeTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Container>
+        <div className="App">
+          <Box component="div" p={5}></Box>
+          <Card>
+            <CardHeader
+              action={
+                <FormGroup>
+                  <FormControlLabel
+                    control={<Switch checked={isDarkTheme} onChange={changeTheme} />}
+                    label="Dark Theme"
+                  />
+                </FormGroup>
+              }
+            />
+            <CardContent>
+              <Typography variant="h3" component="h3">
+                Kindacode.com
+              </Typography>
+              <Typography variant="body1" color='primary.main'>
+                Dark Mode is {isDarkTheme ? "On" : "Off"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
